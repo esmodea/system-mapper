e56c4b0807cdfa281666863c3b20eabbad896ecb
esmodea <esmodearburk@gmail.com>
2026-08-25 19:44:46 -0500

    Implemented a proper welcome splash screen

--- lib/user_interface/widgets/welcome_splash.dart
+++ lib/user_interface/widgets/welcome_splash.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:system_mapper/user_interface/widgets/system_text_button.dart';
 
 class WelcomeSplash extends StatelessWidget {
   final VoidCallback callback;
@@ -6,6 +7,36 @@ class WelcomeSplash extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(onTap: callback, child: Text('Welcome!'));
+    return Container(
+      color: ColorScheme.of(context).primaryContainer,
+      child: Column(
+        children: [
+          Expanded(
+            child: Scaffold(
+              body: Center(
+                child: Text(
+                  'Welcome!',
+                  style: TextTheme.of(context).headlineLarge,
+                ),
+              ),
+              floatingActionButton: FloatingActionButton(
+                onPressed: () {},
+                // backgroundColor: Colors.transparent,
+                child: Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: SystemTextButton(
+                    text: '→',
+                    onPressed: () {
+                      callback();
+                    },
+                    isExpanded: true,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
