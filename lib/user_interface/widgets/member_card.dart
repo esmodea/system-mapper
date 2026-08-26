27151a78054102f12f3486148cc93489f09bdc94
esmodea <esmodearburk@gmail.com>
2026-08-25 21:36:52 -0500

    Updated member card visuals

--- lib/user_interface/widgets/member_card.dart
+++ lib/user_interface/widgets/member_card.dart
@@ -15,11 +15,48 @@ class MemberCard extends StatelessWidget {
           color: ColorScheme.of(context).surface,
         ),
         padding: EdgeInsets.only(left: 10, right: 5, top: 5, bottom: 5),
-        constraints: BoxConstraints(maxWidth: 500, maxHeight: 200),
-        child: Row(
+        constraints: BoxConstraints(maxHeight: 200),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(member!.memberName ?? 'ERROR'),
-            FloatingActionButton(onPressed: () {}, child: Icon(Icons.edit)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  member!.memberName ?? '',
+                  style: TextTheme.of(context).headlineMedium,
+                ),
+                FloatingActionButton(
+                  onPressed: () {
+                    if (member?.inFront ?? false) {
+                      member?.addToFront();
+                    } else {
+                      member?.removeFromFront();
+                    }
+                  },
+                  child: Icon(Icons.edit),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 60),
+              child: Divider(thickness: 2),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: ColorScheme.of(context).primary.withAlpha(100),
+                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                ),
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  member!.memberBio ?? '',
+                  style: TextTheme.of(context).bodyMedium,
+                ),
+              ),
+            ),
           ],
         ),
       );
@@ -27,14 +64,20 @@ class MemberCard extends StatelessWidget {
       return Container(
         decoration: BoxDecoration(
           borderRadius: BorderRadius.all(Radius.circular(20)),
-          color: ColorScheme.of(context).surface,
+          // color: ColorScheme.of(context).surface,
         ),
         padding: EdgeInsets.only(left: 10, right: 5, top: 5, bottom: 5),
-        constraints: BoxConstraints(maxWidth: 500, maxHeight: 200),
+        constraints: BoxConstraints(maxHeight: 200),
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            Text('Add a new member'),
+            Text(
+              'Add a new member',
+              style: TextTheme.of(
+                context,
+              ).bodyMedium?.copyWith(color: ColorScheme.of(context).onPrimary),
+            ),
+            SizedBox(width: 10),
             FloatingActionButton(
               onPressed: () {
                 showModalBottomSheet(
